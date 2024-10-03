ae8532a946090da25a28178f839c6ef886c5a89a
paieer <paieer@users.noreply.github.com>
2024-10-03 10:51:36 +0800

    Update version.go

--- main/version.go
+++ main/version.go
@@ -9,7 +9,7 @@ import (
 
 var cmdVersion = &base.Command{
 	UsageLine: "{{.Exec}} version",
-	Short:     "Show current version of Xray",
+	Short:     "Show current version Xcore",
 	Long: `Version prints the build information for Xray executables.
 	`,
 	Run: executeVersion,
